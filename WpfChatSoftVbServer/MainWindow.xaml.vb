75ddef962758cdbada7ebfc264a6f4689cdd82ec
frostyvalley <frostyvalley@qq.com>
2018-05-26 13:44:15 +0800

    nothing to say...

--- WpfChatSoftVbServer/MainWindow.xaml.vb
+++ WpfChatSoftVbServer/MainWindow.xaml.vb
@@ -1,3 +1,36 @@
-﻿Class MainWindow
+﻿Imports System.Net
+Imports System.Net.Sockets
 
+Class MainWindow
+
+    Private ServerSocket As Socket
+    Private ServerIP As String = "127.0.0.1"
+    Private ServerPort As String = "8009"
+    Private ServerAddress As IPAddress
+    Private ServerEndPoint As IPEndPoint
+
+    Private ConnSocket As Socket
+
+    Public Sub New()
+        StartClient()
+    End Sub
+
+    Private Sub StartClient()
+        Dim clientWindoww As New WpfChatSoftVbClient.MainWindow
+        clientWindoww.Show()
+    End Sub
+
+    Private Sub BtnStartServer_Click(sender As Object, e As RoutedEventArgs) Handles BtnStartServer.Click
+
+    End Sub
+
+    Private Sub BtnSendMsg_Click(sender As Object, e As RoutedEventArgs) Handles BtnSendMsg.Click
+        ServerAddress = IPAddress.Parse(ServerIP)
+        ServerEndPoint = New IPEndPoint(ServerAddress, ServerPort)
+        ShowMsg(ServerEndPoint.ToString)
+    End Sub
+
+    Private Sub ShowMsg(ByVal msg As String)
+        TxtShow.AppendText(msg & vbCrLf)
+    End Sub
 End Class
